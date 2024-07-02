fe448840a5002a47a2991d479acdb8ed3f59b408
manuel-lopez <manuel.lopez@airbnb.com>
2024-07-01 18:09:49 -0700

    Test case and initial code for unusedFileprivate rule

--- Tests/RulesTests+Redundancy.swift
+++ Tests/RulesTests+Redundancy.swift
@@ -137,6 +137,21 @@ class RedundancyTests: RulesTests {
         testFormatting(for: input, rule: FormatRules.redundantFileprivate, options: options)
     }
 
+    func testRemoveUnusedFilePrivate() {
+        let input = """
+        struct Foo {
+            fileprivate var foo = "foo"
+            var bar = "bar"
+        }
+        """
+        let output = """
+        struct Foo {
+            var bar = "bar"
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.unusedFileprivate)
+    }
+
     func testFileprivateVarChangedToPrivateIfNotAccessedFromAnotherType() {
         let input = """
         struct Foo {
